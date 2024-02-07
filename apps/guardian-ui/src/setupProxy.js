9f4224f4a2f80c4bee6eca44b4153d93db4bd47c
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-02-06 22:36:41 -0500

    chore: dev server runtime config.json

--- apps/guardian-ui/src/setupProxy.js
+++ apps/guardian-ui/src/setupProxy.js
@@ -1,17 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const config_path = process.env.REACT_APP_FM_CONFIG_PATH
-  ? process.env.REACT_APP_FM_CONFIG_PATH
+const config_path = process.env.REACT_APP_DEV_CONFIG_PATH
+  ? process.env.REACT_APP_DEV_CONFIG_PATH
   : 'ws://127.0.0.1';
 
 module.exports = function (app) {
+  console.log(`serving up ${config_path} for frontend`);
   app.use(
     '/config',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://localhost:5000/dev_config',
       changeOrigin: true,
       pathRewrite: {
-        '^/config.json': config_path,
+        'config/config.json': config_path,
       },
     })
   );
